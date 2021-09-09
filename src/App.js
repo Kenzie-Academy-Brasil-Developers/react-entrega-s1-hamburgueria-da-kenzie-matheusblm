b5a391465bdb552873b20de4576ab44f2523f967
hank <matheus_bueno10@hotmail.com>
2021-09-09 15:02:00 -0300

    Arrumado quuestao do carrinho

--- src/App.js
+++ src/App.js
@@ -31,32 +31,31 @@ function App() {
         { id: 7, name: "Fanta", category: "Bebidas", price: 4.99 },
       ]);
     } else {
-      const filter = products.filter(
-        (elemento) => elemento.name.toLowerCase() === newItem
+      const filter = products.filter((elemento) =>
+        elemento.name.toLowerCase().includes(newItem)
       );
-      console.log(item);
+
       setProducts(filter);
     }
   };
   function handleClick(ids) {
     const finder = products.find((el) => el.id === ids);
-    setCurrentSale([...currentSale, finder]);
-    setCartTotal(
-      currentSale
-        .reduce(function (acumulador, valorAtual) {
-          return acumulador + valorAtual.price;
-        }, 0)
-        .toFixed(2)
-    );
+    if (!currentSale.includes(finder)) {
+      setCurrentSale([...currentSale, finder]);
+    }
   }
-
+  const total = currentSale
+    .reduce(function (acumulador, valorAtual) {
+      return acumulador + valorAtual.price;
+    }, 0)
+    .toFixed(2);
   return (
     <div className="App">
       <header className="App-header">
         <h3>Filtrar itens</h3>
         <Filter showProducts={showProducts} />
         <MenuContainer products={products} handleClick={handleClick} />
-        <h4>Subtotal - R$ {cartTotal}</h4>
+        <h4>Subtotal - R$ {total}</h4>
         <Cart currentSale={currentSale} setCurrentSale={setCurrentSale} />
       </header>
     </div>
